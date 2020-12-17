971df7672b2a04f14c7fe1184ccda8f99c1668b4
rithik112 <rithiksarab112@gmail.com>
2020-12-18 00:43:39 +0530

    Method to Retrive the whole List

--- Server/routes/list.js
+++ Server/routes/list.js
@@ -3,4 +3,11 @@ var router = express.Router();
 
 var list = require('../models/list');
 
+router.get('/', (req, res) => {
+    list.find((err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log('Error while Retriving List :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
 module.exports = router;
