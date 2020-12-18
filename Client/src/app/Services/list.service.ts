cf78fd0fa0690e3c3016c1b02b90ef9708df563f
rithik112 <rithiksarab112@gmail.com>
2020-12-18 12:11:51 +0530

    Basic Setup

--- Client/src/app/Services/list.service.ts
+++ Client/src/app/Services/list.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { List } from './list.model';
 @Injectable({
   providedIn: 'root'
 })
-export class ListService {
 
-  constructor() { }
+export class ListService {
+  selectedlist: List;
+  lists: List[];
+  readonly baseURL = 'http://localhost:3000/lists';
+
+  constructor(private http: HttpClient) { }
 }
